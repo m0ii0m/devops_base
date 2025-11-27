9cb574a803ea3ddc51e33b7289e5d20abb131803
Grégoire <gregoire.lefevre@edu.esiee.fr>
2025-11-27 13:43:24 +0100

    added automated test section

--- td4/scripts/sample-app/app.js
+++ td4/scripts/sample-app/app.js
@@ -1,10 +1,15 @@
 // app.js
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8080;
 app.get('/', (req, res) => {
 res.send('Hello, World!');
 });
-app.listen(port, () => {
-console.log(`Example app listening on port ${port}`);
+app.get('/name/:name', (req, res) => {
+res.send(`Hello, ${req.params.name}!`);
+});
+module.exports = app;
+
+app.get('/name/:name', (req, res) => {
+  const name = req.params.name; 
+  res.send(`Hello, ${name}!`);
 });
