d96add7437d17e66925685e28d480adaf065b20f
m0ii0m <ludovic.regnier.2004@gmail.com>
2026-02-02 19:40:09 +0100

    Update CI/CD pipeline to deploy on a self-hosted server

--- infra/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_region" {
-  description = "AWS Region"
-  default     = "us-east-2"
-}
-
-variable "instance_type" {
-  description = "EC2 Instance Type"
-  default     = "t3.small"
-}
